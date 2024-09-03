a799d531c3af1dfe2f676571f81b5bff6dff2107
kozuuuu <06jaysang2002@gmail.com>
2024-09-03 11:10:39 +0800

    learning Java with w3school

--- LearnJava.java
+++ LearnJava.java
@@ -243,3 +243,55 @@ comment*/
 
 
 // == JAVA METHODS == JAVA METHODS == JAVA METHODS == JAVA METHODS == JAVA METHODS == JAVA METHODS == JAVA METHODS == JAVA METHODS
+//A. Insert the missing part  to call myMethod from main
+    static void myMethod() {
+        System.out.println("I just got executed!");
+    }
+    
+    public static void main(String[] args) {
+        
+    myMethod()
+    ;
+    }
+
+//B .Insert the missing part to call myMethod from main two times.
+    static void myMethod() {
+        System.out.println("I just got executed!");       
+    }
+    public static void main(String[] args) {
+        myMethod();
+        myMethod();
+    }
+
+//C. Add a "fname" parameter of type String to myMethod, and output "John Doe".
+    static void myMethod(String fname) {
+        System.out.println(fname + " Doe");
+    }
+    public static void main(String[] args) {
+        myMethod("John");
+    }
+
+//D. Insert the missing part to print the number 8 in main, by using a specific keyword inside myMethod: "RETURN"
+    static int myMethod(int x) {
+        return 5 + x;
+    }
+    public static void main(String[] args) {
+      System.out.println(myMethod(3));
+    }
+    
+//E. Follow the comments to insert the missing parts of the code below:
+// Create a checkAge() method with an integer variable called age
+    static void checkAge
+        (int age) {
+        // If age is less than 18, print "Access denied"
+        if ( age < 18 ) {
+            System.out.println("Access denied"); 
+        // If age is greater than, or equal to, 18, print "Access granted"
+        } else {
+            System.out.println("Access granted"); 
+        }
+    } 
+    public static void main(String[] args) { 
+        // Call the checkAge method and pass along an age of 20
+        checkAge(20);
+    }
