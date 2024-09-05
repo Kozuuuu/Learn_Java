737534c68fa9f00b2704ba0e163647d2bb92c037
kozuuuu <06jaysang2002@gmail.com>
2024-09-05 11:57:49 +0800

    not working

---
+++ MainI.java
@@ -0,0 +1,17 @@
+
+public class MainI {
+    class Maini {
+        int i;
+        Maini(int i) {
+            this.i = i;
+        }
+        
+        void j() {
+            System.out.println(i);
+        }
+    }    
+    public static void main(String[] args) {
+        Maini() = new Maini(10);
+        obj.j();
+    }
+}
