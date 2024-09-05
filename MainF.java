e963cde5fb3540a6603be361b9c59ae3431989e4
kozuuuu <06jaysang2002@gmail.com>
2024-09-05 11:58:22 +0800

    preparing for Evaluation Exam

--- MainF.java
+++ MainF.java
@@ -3,10 +3,6 @@ public class MainF {
         System.out.println(new StringBuilder(message).reverse().toString());
     }
     public static void main(String[] args) {
-        // new MainA().a(5);
-        // new MainC().c(5);
-        // System.out.println(new MainD().d(5));
-        // new MainE().e();
         new MainF().f("Hello World");
     }           
 }
